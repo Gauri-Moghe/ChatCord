cef461cff8983159cfba52b19f2ce473e83c9ff3
Gauri-Moghe <gauri.s.moghe@gmail.com>
2026-01-07 16:35:30 -0800

    Adding message utilities and timestamps

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path  = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -10,20 +11,22 @@ const io = socketio(server);
 //set static folder for frontend
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot'; //bot name
+
 //Run when the client connects
 io.on('connection', socket => {
 
     // console.log('New WS Connection...');
 
     //Welcoming the current user
-    socket.emit('message', 'Welcome to ChatCord!');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
 
     //this code runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName,'A user has left the chat'));
 
     });
 
